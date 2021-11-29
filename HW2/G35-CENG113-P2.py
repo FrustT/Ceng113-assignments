4304085ee77925a363ef6cd02159b511838e67cc
Ege Önder <40398628+EgeOnder@users.noreply.github.com>
2021-11-29 12:06:02 +0300

    Added some unnecessary comments and fixed unnecessary things 🎉

--- HW2/G35-CENG113-P2.py
+++ HW2/G35-CENG113-P2.py
@@ -5,179 +5,201 @@
 #                                                                                        #
 ##########################################################################################
 
-#We declare our global variables so we can use them later.
-not_in_correct_form = False
-total = 0
-carry_out = 0
-computed = False
-ans = "0"
+# we declare our global variable
+# so we can use them later
+ans = 0
 
-
-#This statement ensures that program keeps running till user enters "2" to exit
-while not ans == "2":
-
+# this statement ensures that program keeps
+# running till user enters "2" to exit
+while not ans == 2:
     print("Welcome to FullAdder!\n(1) Compute and Display the Outputs\n(2) Exit") 
-    ans = input("You choose: ")
+    ans = int(input("You choose: "))
 
+    # with this flag, we will check if computation
+    # of entered input is necessary
     computed = False
 
-    if ans == "1":
-        print(f"You have chosen option {ans}")
-
-        #This statement ensures that after calculation executed,User isn't asked to enter a base
+    if ans == 1:
+        # this statement ensures that after calculation executed,
+        # user isn't asked to enter a base
         while not computed:
             base = int(input("Which base will you use to enter data lines (base 16/8/2)? "))
 
             if base == 2:
 
-                #This statement ensures that after calculation executed,User isn't asked to enter a number
+                # this statement ensures that after calculation executed,
+                # user isn't asked to enter a number
                 while not computed:
-                    number = input("Please enter input:")
+                    number = input("Please enter input: ")
 
+                    # with this flag, we will check if the value
+                    # entered is worthy enough to compute
                     not_in_correct_form = False
 
+                    # since the biggest value we can compute is binary 111
+                    # this code checks if the number is in acceptable range
+                    if len(number) > 3: 
+                        print("Please enter a value with max 3 digits!")
+
+                        not_in_correct_form = True
 
                     for digit in number:
-
-                            #This loop takes each char in string and checks if this char is in string below 
-                            # if it is not then it is not in binary 
-                            if not digit in "01":
-                                print("Your input is not in binary!",end= '')
-
-                                not_in_correct_form = True
-
-                    #This statement ensures that if an invalid input is entered, code below doesn't throw an error
-                    if not_in_correct_form is False:
-                        
-                        #Since the biggest value we can compute is binary 111
-                        #This code checks if the number is in acceptable range
-                        if len(number) > 3: 
-                            print("Please enter a value with max 3 digits!",end= '')
+                        # This loop takes every digit in string and checks
+                        # if this digit is in string below
+                        # if not, then it is not in binary
+                        if not digit in "01":
+                            print("Your input is not in binary!")
 
                             not_in_correct_form = True
+
+                            # if any found, break out of the loop
+                            # since it's not necessary to look after
+                            break
+
                                     
-                    if not_in_correct_form:
-                        print("Please try again!")
+                    # checking if it's not in correct form
+                    # if not, then no need to compute
+                    if not not_in_correct_form:
 
-                    else:
-
-                        #Since our code needs 3 digit to execute,
-                        #This code converts smaller valid unexecutable inputs to executable one (e.g 11 => 011)
+                        # since our code needs 3 digit to execute,
+                        # this code converts smaller valid unexecutable
+                        # inputs to executable one (e.g 11 => 011)
                         while len(number) < 3:
                             number = "0" + number
 
+                        # defining our A, B and C values for base 2
                         A = int(number[0])
                         B = int(number[1])
                         C = int(number[2])
 
+                        # saying it's ready to compute
                         computed = True
-
 
             elif base == 8:
 
-                #This statement ensures that after calculation executed,User isn't asked to enter a number
+                # this statement ensures that after calculation executed,
+                # user isn't asked to enter a number
                 while not computed:
-                    number = input("Please enter input:")
+                    number = input("Please enter input: ")
 
+                    # with this flag, we will check if the value
+                    # entered is worthy enough to compute
                     not_in_correct_form = False
 
-                    #This loop takes each char in string and checks if this char is in string below 
-                    #If it is not then it is not in base 8 
                     for digit in number:
-
-                              
-                            if not (digit in "01234567"): 
-                                print("Your input is not in base 8.",end='')
-
-                                not_in_correct_form = True
-                    
-                    #This statement ensures that if an invalid input is entered ,code below doesn't throw an error
-                    if not_in_correct_form is False :
-
-                        #Since the biggest value we can compute is binary 111
-                        #This code checks if the number is in acceptable range
-                        if (len(number) > 1 or int(number) > 7):
-                            print(f"Octal {number} cannot be represented with 3 bits!",end='')# https://en.wikipedia.org/wiki/Octal#:~:text=Binary%20to%20octal%20conversion%5Bedit,Octal%20to%20hexadecimal%20conversion
+                        # This loop takes every digit in string and checks
+                        # if this digit is in string below
+                        # if not, then it is not in base 8
+                        if not (digit in "01234567"): 
+                            print("Your input is not in base 8!")
 
                             not_in_correct_form = True
 
+                            # if any found, break out of the loop
+                            # since it's not necessary to look after
+                            break
+
+                    if not not_in_correct_form:
+                        # since the biggest value we can compute is binary 111
+                        # this code checks if the number is in acceptable range
+                        if (len(number) > 1 or int(number) > 7):
+                            print(f"Octal {number} cannot be represented with 3 bits!")
+
+                            not_in_correct_form = True
                     
-                    if not_in_correct_form:
-                        print("Please try again!")
-                    else:
+                    # checking if it's not in correct form
+                    # if not, then no need to compute
+                    if not not_in_correct_form:
+
+                        # converting into integer since
+                        # we haven't done it at the beginning
                         number = int(number)
+
+                        # defining our A, B and C values for base 8
                         C = number % 2
                         B = (number % 4 - C )// 2
                         A = number // 4
 
+                        # saying it's ready to compute
                         computed = True
 
 
             elif base == 16:
 
-                #This statement ensures that after calculation executed,User isn't asked to enter a number
+                # this statement ensures that after calculation executed,
+                # user isn't asked to enter a number
                 while not computed:
-                    number = input("Please enter input:").upper()
 
+                    # we are using upper here because in base 16,
+                    # value entered could be an hexadecimal letter
+                    number = input("Please enter input: ").upper()
+
+                    # with this flag, we will check if the value
+                    # entered is worthy enough to compute
                     not_in_correct_form = False
 
-                    #This loop takes each char in string and checks if this char is in string below 
-                    #If it is not than it is not in base 16
+                    # since the biggest value we can compute is binary 111
+                    # this code checks if the number is in acceptable range
+                    if(len(number) > 1 or "0123456789ABCDEF".index(number) > 7):
+                        print(f"Hexadecimal {number} can not be represented with 3 bits!")
+
+                        not_in_correct_form = True
+
                     for digit in number:
-
-                               
-                            if not (digit in "0123456789ABCDEF"): 
-                                print("Your input is not in base 16.",end=' ')
-
-                                not_in_correct_form = True
-                  
-                    #This statement ensures that if an invalid input is entered ,code below doesn't throw an error
-                    if not_in_correct_form is False :
-
-                        
-                        #Since the biggest value we can compute is 111(binary)
-                        #This code checks if the number is in acceptable range
-                        if(len(number) > 1 or "0123456789ABCDEF".index(number) > 7):
-                            print(f"Hexadecimal {number} can not be represented with 3 bits!",end='')
+                        # This loop takes every digit in string and checks
+                        # if this digit is in string below
+                        # if not, then it is not in base 16
+                        if not (digit in "0123456789ABCDEF"): 
+                            print("Your input is not in base 16!")
 
                             not_in_correct_form = True
 
-                    if not_in_correct_form:
-                        print("Please try again!")
-                    else:
-                        intNumber = int(number)
-                            
-                        C = intNumber % 2
-                        B = (intNumber % 4 - C )// 2
-                        A = intNumber // 4
+                            # if any found, break out of the loop
+                            # since it's not necessary to look after
+                            break
 
+                    # checking if it's not in correct form
+                    # if not, then no need to compute
+                    if not not_in_correct_form:
+
+                        # converting into integer since
+                        # we haven't done it at the beginning
+                        number = int(number)
+                            
+                        # defining our A, B and C values for base 16
+                        C = number % 2
+                        B = (number % 4 - C )// 2
+                        A = number // 4
+
+                        # saying it's ready to compute
                         computed = True
 
-            #Since previous statements covers all the options,
-            #If they are all False then that means input is invalid.
+            # since previous statements covers all the options,
+            # if they are all 'false', then that means input is invalid.
             else:
                 print("Invalid input, please enter again.")
 
-            #This statement ensures that computation doesn't executed unnecessarily    
+            # this statement ensures that
+            # computation doesn't executed unnecessarily 
             if computed:
+                carry_out = 0
+
+                # find raw sum of bits
                 total = A + B + C
 
-                if total == 3:
-                    total = 1
-                    carry_out = 1        
-                elif total == 2:
-                    total = 0
-                    carry_out = 1
+                # perform an operation to determine
+                # carry out and sum values
+                carry_out = total // 2
+                total = abs(total - 2)
 
-                print(f"Sum is {total} C_out is {carry_out}")
+                # print the result!
+                print(f"Sum: {total}\nC_out: {carry_out}")
                 
 
-    elif ans == "2":
-        print(f"You have chosen option {ans}")
+    elif ans == 2:
+        print("Byee :(")
 
-    #Since previous statements covers all the options,
-    #If they are all False then that means input is invalid.    
+    # since previous statements covers all the options,
+    # if they are all 'false', then that means input is invalid.  
     else :
         print("Invalid input, please enter again.")
-
-print("Byee!!")
