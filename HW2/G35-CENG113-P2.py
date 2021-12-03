95bc0c855e130a9972d7bb489c4ec9ecf5e7c524
FrustT <burak.erinc98@gmail.com>
2021-12-03 10:38:04 +0300

    debugging

--- HW2/G35-CENG113-P2.py
+++ HW2/G35-CENG113-P2.py
@@ -7,25 +7,24 @@
 
 # we declare our global variable
 # so we can use them later
-ans = 0
+ans = "0"
 
 # this statement ensures that program keeps
 # running till user enters "2" to exit
-while not ans == 2:
+while not ans == "2":
     print("Welcome to FullAdder!\n(1) Compute and Display the Outputs\n(2) Exit") 
-    ans = int(input("You choose: "))
+    ans = input("You choose: ")
 
-    # with this flag, we will check if computation
-    # of entered input is necessary
+    #This line sets flags to default value for healthy loops
     computed = False
 
-    if ans == 1:
+    if ans == "1":
         # this statement ensures that after calculation executed,
         # user isn't asked to enter a base
         while not computed:
-            base = int(input("Which base will you use to enter data lines (base 16/8/2)? "))
+            base = input("Which base will you use to enter data lines (base 16/8/2)? ")
 
-            if base == 2:
+            if base == "2":
 
                 # this statement ensures that after calculation executed,
                 # user isn't asked to enter a number
@@ -62,8 +61,8 @@ while not ans == 2:
                     if not not_in_correct_form:
 
                         # since our code needs 3 digit to execute,
-                        # this code converts smaller valid unexecutable
-                        # inputs to executable one (e.g 11 => 011)
+                        # this code converts smaller valid unexecutable inputs
+                        # to executable one (e.g 11 => 011)
                         while len(number) < 3:
                             number = "0" + number
 
@@ -75,7 +74,7 @@ while not ans == 2:
                         # saying it's ready to compute
                         computed = True
 
-            elif base == 8:
+            elif base == "8":
 
                 # this statement ensures that after calculation executed,
                 # user isn't asked to enter a number
@@ -87,8 +86,8 @@ while not ans == 2:
                     not_in_correct_form = False
 
                     for digit in number:
-                        # This loop takes every digit in string and checks
-                        # if this digit is in string below
+                        # This loop takes every digit in string and checks if
+                        #this digit is in string below
                         # if not, then it is not in base 8
                         if not (digit in "01234567"): 
                             print("Your input is not in base 8!")
@@ -124,7 +123,7 @@ while not ans == 2:
                         computed = True
 
 
-            elif base == 16:
+            elif base == "16":
 
                 # this statement ensures that after calculation executed,
                 # user isn't asked to enter a number
@@ -138,13 +137,6 @@ while not ans == 2:
                     # entered is worthy enough to compute
                     not_in_correct_form = False
 
-                    # since the biggest value we can compute is binary 111
-                    # this code checks if the number is in acceptable range
-                    if(len(number) > 1 or "0123456789ABCDEF".index(number) > 7):
-                        print(f"Hexadecimal {number} can not be represented with 3 bits!")
-
-                        not_in_correct_form = True
-
                     for digit in number:
                         # This loop takes every digit in string and checks
                         # if this digit is in string below
@@ -157,6 +149,14 @@ while not ans == 2:
                             # if any found, break out of the loop
                             # since it's not necessary to look after
                             break
+                            
+                    if not not_in_correct_form:
+                        # since the biggest value we can compute is binary 111
+                        # this code checks if the number is in acceptable range
+                        if(len(number) > 1 or "0123456789ABCDEF".index(number) > 7):
+                            print(f"Hexadecimal {number} can not be represented with 3 bits!")
+
+                            not_in_correct_form = True
 
                     # checking if it's not in correct form
                     # if not, then no need to compute
@@ -190,16 +190,17 @@ while not ans == 2:
                 # perform an operation to determine
                 # carry out and sum values
                 carry_out = total // 2
-                total = abs(total - 2)
+                total = total - carry_out * 2 
 
                 # print the result!
                 print(f"Sum: {total}\nC_out: {carry_out}")
                 
 
-    elif ans == 2:
+    elif ans == "2":
         print("Byee :(")
 
     # since previous statements covers all the options,
     # if they are all 'false', then that means input is invalid.  
     else :
         print("Invalid input, please enter again.")
+        
