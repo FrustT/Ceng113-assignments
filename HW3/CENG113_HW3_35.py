9ec342b2537b0a39826051b65a04b606d77e38d2
Ege Önder <40398628+EgeOnder@users.noreply.github.com>
2021-12-13 12:53:12 +0300

    added manual inputs and removed herobrine

--- HW3/CENG113_HW3_35.py
+++ HW3/CENG113_HW3_35.py
@@ -4,90 +4,91 @@ scoreboard = [[0 for i in range(10)], [0 for i in range(10)]]
 ballScores = [[], []]
 gameOver = [False, False]
 extraRolls = [0, 0]
-frameSums= [0,0]
-framePointers = [0,0]
-playersName = ['A','B']
+frameSums= [0, 0]
+framePointers = [0, 0]
+playersName = ['A', 'B']
 
+pinsUp = 10
+
+def enter_pins(player):
+    pins = 0
+ ###INVALID INPUT'A BAK
+    while pins <= pinsUp:
+        pins = int(input(f'Please enter pins(0-{pinsUp}):'))
+        pinsUp -= pins
+        
+    return pins
 
 def print_scoreboard(player):
-    global scoreboard
-
     print("Ball scores: ",*ballScores[player])
 
     print("Total Scores:", end=" ")
-    print(*scoreboard[player],sep = '|')
-
+    print(*scoreboard[player], sep = " | ")
 
 def calculate_frame(player):
-    scoreboard[player] [framePointers[player]] += frameSums[player]
+    scoreboard[player][framePointers[player]] += frameSums[player]
 
     if framePointers[player] != 9: framePointers[player] += 1
-    
-    else : gameOver[player] = True
+    else: gameOver[player] = True
 
-def extra_roll(player): #roll için daha iyi bir isim lazım
-    roll= 0
+def extra_roll(player):
+    pins = 0
 
-    for a in range(extraRolls[player]):
-        roll= random.randrange(0,10-roll)
+    for i in range(extraRolls[player]):
+        pins = int(input())#ENTER PINSI ÇAĞIR
 
-        rolled(player,roll)
+        rolled(player, roll)
 
     calculate_frame(player)
     extraRolls[player] = 0
 
-
 def rolled(player,pins):
-    print(f"Pins:{pins}")
     ballScores[player].append(pins)
     frameSums[player] += pins
 
+def roll(player):
+    print(f"Player {playersName[player]} rolls...")
 
-def Roll(player): #THIS IS NO EXTRA ROLL SEQUENCE
-     print(f"Player {playersName[player]} rolls...")
+    firstRoll = enter_pins(player)
+    rolled(player, firstRoll)
 
-     firstRoll= random.randrange(0,10)
-     rolled(player,firstRoll)
+    if firstRoll != 10:
+        secondRoll = int(input("Pins: "))
 
-     if firstRoll != 10:
-          secondRoll= random.randrange(0,10-firstRoll)
-          rolled(player,secondRoll)
+        rolled(player,secondRoll)
 
-          if firstRoll + secondRoll == 10 : #SPARE
-               print("Spare")
-               extraRolls[player] = 1
+        if firstRoll + secondRoll == 10 :
+            extraRolls[player] = 1
+            
+            if framePointers[player] == 9: extra_roll(player)  
 
-          else :#OPEN FRAME
-               calculate_frame(player)
+        else:
+            calculate_frame(player)
 
-     else :#STRİKE
-          print(f"Strike!!!")
+    else:
+        extraRolls[player] = 2
         
-          extraRolls[player] = 2
+        if framePointers[player] == 9: 
+            extraRolls[player] = 1
+            extra_roll(player)
 
-          
-     print_scoreboard(player)
-
+    print_scoreboard(player)
 
 def main():
+    turn = random.randint(0, 1)
 
-    turn = random.randint(0,1)
+    while not all(gameOver):
+        if extraRolls[turn] != 0: extra_roll(turn)
+        else: roll(turn)
 
-    while not all(gameOver):#this loop keeps repeating till both player's game is over
-
-        if extraRolls[turn] != 0:extra_roll(turn)
-        else :Roll(turn)
-
-        if not gameOver[turn -1] : turn = (turn+1) % 2
+        if not gameOver[not turn]: turn = not turn
     else:
         print(f"Game Over\n__________")
         
         for playersScores in scoreboard:
             print(f"Player {playersName[scoreboard.index(playersScores)]} got {playersScores[-1]} points")
 
-        winner = 1 if scoreboard[0][-1]> scoreboard[1][-1] else 0
+        winner = 1 if scoreboard[0][-1] > scoreboard[1][-1] else 0
         print(f"{playersName[winner]} Have WON!!!")
-    
-
 
 main()
