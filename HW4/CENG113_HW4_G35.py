73d685155e30f2964b3f47039cec05de5edc0c05
FrustT <burak.erinc98@gmail.com>
2022-01-02 18:26:20 +0300

    added get_sentence  and clean_sentence functions , debugged some , added Herobrine (2.01.2022)

--- HW4/CENG113_HW4_G35.py
+++ HW4/CENG113_HW4_G35.py
@@ -1,8 +1,8 @@
 ###########################################
-#      Group-35                           #
-#        290201099 - Burak ERİNÇ          #
-#        290201082 - Arif Ege ÖNDER       #
-#                                         #
+#      Group-35                                                                    #
+#        290201099 - Burak ERİNÇ                                        #
+#        290201082 - Arif Ege ÖNDER                                  #
+#                                                                                         #
 ###########################################
 
 def read_genes(file_path):
@@ -36,7 +36,7 @@ def get_fragments(gene_dict, frag_len=50):
         lengths = key.split("|")[1].split("-")
 
         # find the length with substracting both key values
-        key_length = int(lengths[1]) - int(lengths[0])
+        key_length = int(lengths[1]) - int(lengths[0]) + 1
 
         # only execute if frag_len is bigger than the length
         # to filter the shorter ones
@@ -62,22 +62,21 @@ def get_fragments(gene_dict, frag_len=50):
 
     return new_dict
 
+def get_similarity(s1, s2):
+    similar = 0
+
+    # get similarity with checking every character's
+    # position and value. if same, increment similar
+    for i in range(len(s1)):
+        if s1[i] == s2[i]: similar += 1
+
+    # lastly, divide similar to length of a sequence
+    # to find the percent with format 0.x
+    return similar / len(s1)
+
 def filter_frags(frag_dict, threshold=0.7):
     dissimilar_frag_dict = {}
     added = []
-
-    def get_similarity(s1, s2):
-        similar = 0
-
-        # get similarity with checking every character's
-        # position and value. if same, increment similar
-        for i in range(len(s1)):
-            if s1[i] == s2[i]: similar += 1
-
-        # lastly, divide similar to length of a sequence
-        # to find the percent with format 0.x
-        return similar / len(s1)
-
     # check for every key with every present
     # in the dictionary with time complexity of O(n^2)
     for key in frag_dict:
@@ -102,23 +101,50 @@ def filter_frags(frag_dict, threshold=0.7):
     return dissimilar_frag_dict
 
 def get_sentences(dissimilar_frag_dict):
+    sentences_dict = {}
+
     def generate_kmers(seq, k):
-        # STEP 4.a
-        pass
-    # STEP 4.b
+        kmers= ""
+
+        # this code takes a string and splits it into k length strings and 
+        # add them in order to another string
+        for i in range(0,len(seq)- k + 1):
+            kmers += " " + seq[i:i+k]
+        return kmers
+        
+    # this block takes all keys and values and
+    # assings kmers to its key value in another dict
+    for key in dissimilar_frag_dict:
+        sentences_dict[key] = generate_kmers(dissimilar_frag_dict[key],4).lstrip()
+    return sentences_dict
 
 def clean_dict(sentences_dict):
+    cleaned_dict = {}
+
     def clean_sentence(sentence):
-        # STEP 5.a
-        pass
-    # STEP 5.b
+        cleaned_sentence_list = []
+        for word in sentence.split():
+            if word not in cleaned_sentence_list:
+                cleaned_sentence_list.append(word)
+        return " ".join(cleaned_sentence_list)
+    for key in sentences_dict:
+        cleaned_dict[key] = clean_sentence(sentences_dict[key])
+    return cleaned_dict
+
+def get_wordNumber(dict):
+    wordNumber = 0
+
+    # this block gets total word number in  a dict
+    for key in dict:
+        # +1 is for first word that has no blank spaces before it
+        wordNumber +=(len(dict[key])+1)/ 5
+    return wordNumber
 
 def write_genes(file_path, clean_sentences_dict):
     # STEP 6
     pass
 
 def main():
-
     # STEP 7: Runs required steps and prints data statistics
 
         # 1) read genes from input.txt
@@ -135,11 +161,15 @@ def main():
         print(f"Number of dissimilar fragments: {len(dissimilar_frag_dict)}")
         # 4) get sentences for dissimilar fragments
         #    print the number of words in a sentence -> Expected output: 46
+        sentences_dict = get_sentences(dissimilar_frag_dict)
         # 5) remove duplicate words in sentences
+        cleaned_dict = clean_dict(sentences_dict)
         #    print the total number of words removed -> Expected output: 8194
+        get_wordNumber(sentences_dict) - get_wordNumber(cleaned_dict)
         # 6) write sentences into output.csv
 
     # DO NOT SCAN OR PRINT EXTRA INFORMATION. JUST THE STATS LISTED ABOVE.
 
-
 main()
+
+
